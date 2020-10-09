de17e5f838f6c3574bc131e446c24532fc96519b
tal <alfital2@gmail.com>
2020-10-09 23:13:34 +0300

    allowing to push new TODO to the screen

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,25 +1,67 @@
-
+let p=console.log;
 class TodoList{
 
 }
 
 class TodoItem{
 
-  constructor(task){
-    console.log(this.createUniqueIdForTask());
+constructor(hook,task){
+  this.hookid=hook;
+  this.item = this.extractDateFromTaskObject(task);
+  this.verifyIfDateIsSet();
+  const uidForTask = this.createUniqueIdForTask();
+  this.item = this.setIdForTask(uidForTask);
+  this.render();
+  }
+  
+  verifyIfDateIsSet(){
+    if(this.item['taskDate']==''){
+    const itemProperties={...this.item};
+      itemProperties['taskDate']="No expiration date";
+      this.item = itemProperties;
+    }
+  }
+
+  setIdForTask(id){
+    const itemProperties={...this.item};
+    itemProperties['uid'] = id;
+    return itemProperties;
+  }
+  
+  extractDateFromTaskObject(task){
+
+    const itemProperties={...task};
+    Object.keys(itemProperties).map(function(key, value) {
+      itemProperties[key] = itemProperties[key].value;
+    });
+    return itemProperties;
   }
 
   createUniqueIdForTask(){
     let dt = new Date().getTime();
     let uniqueId = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) =>{
-        let remainder = (dt + Math.random()*16)%16 | 0;
-        dt = Math.floor(dt/16);
+      let remainder = (dt + Math.random()*16)%16 | 0;
+      dt = Math.floor(dt/16);
         return (c=='x' ? remainder :(remainder&0x3|0x8)).toString(16);
     });
     return uniqueId;
-}
+  }
 
+  render(){
+    const unorderedListOftasks = document.querySelector('ul');
+    const newEl = document.createElement('li');
+    newEl.className="card";
+    newEl.innerHTML=`
 
+          <h2>${this.item["taskName"]}</h2>
+          <p>${this.item["taskInfo"]}.</p>
+          <h3>${this.item["taskDate"]}</h3>
+          <h4>${this.item["uid"]}</h4>
+          <button>Finish</button
+
+  `;
+  unorderedListOftasks.append(newEl);
+  }
 }
 
 
@@ -46,7 +88,6 @@ creatFields(){
   elements['taskName'] = document.createElement("INPUT");
   elements['taskInfo'] = document.createElement("INPUT");
   elements['taskDate']= document.createElement("INPUT");
-  //elements['submit']=  document.createElement("INPUT");
 
   this.inputElements= elements;
 }
@@ -57,8 +98,14 @@ initializeElements(){
   this.inputElements['taskInfo'].setAttribute("type", "text");
   this.inputElements['taskInfo'].placeholder="task info(optional)";
   this.inputElements['taskDate'].setAttribute("type", "date");
-  // this.inputElements['submit'].setAttribute("type", "submit");
-  // this.inputElements['submit'].addEventListener('click',()=>App.pushNewElement(this.inputElements));
+
+
+  ////////this is for tasting - delete this!
+  this.inputElements['taskInfo'].value = "asfdsdfgsadfgsdf";
+  this.inputElements['taskName'].value = "asdasd";
+    ////////this is for tasting - delete this!
+
+
 }
 
 render(){
@@ -86,14 +133,19 @@ class App {
     wrapper.id ="wrapper";
     app.append(wrapper);
 
+
+    ///TODO DELETE
+    const TEST_DELETE = document.createElement('ul');
+    app.append(TEST_DELETE);
+    ///TODO DELETE
+
     new InputTask('wrapper');
   }
 
   static pushNewElement(task){
-    const newItem = new TodoItem(task);
-    for (const field in task){
-    console.log(task[field].value);
-    }
+
+    const newItem = new TodoItem('ul',task);
+
   }
 }
 
