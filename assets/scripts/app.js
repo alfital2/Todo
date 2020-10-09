926b486c21fc9805c47f2f1140b50bff64082618
tal <alfital2@gmail.com>
2020-10-09 18:30:54 +0300

    tiny refactor for the class InputTask, and creating new class structure only

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,28 +1,52 @@
+
+class TodoList{
+
+}
+
+class TodoItem{
+
+  constructor(task){
+    console.log(this.createUniqueIdForTask());
+  }
+
+  createUniqueIdForTask(){
+    let dt = new Date().getTime();
+    let uniqueId = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) =>{
+        let remainder = (dt + Math.random()*16)%16 | 0;
+        dt = Math.floor(dt/16);
+        return (c=='x' ? remainder :(remainder&0x3|0x8)).toString(16);
+    });
+    return uniqueId;
+}
+
+
+}
+
+
 class InputTask{
 
-
 inputElements;
+hookid;
 
-constructor(){
+constructor(hookid){
+  this.hookid =hookid;
   this.creatFields();
   this.initializeElements();
   this.render();
 
 }
 
-
 creatFields(){
   const elements = {
     taskName:"",
     taskInfo:"",
-    taskDate:"",
-    submit:""
+    taskDate:""
   }
 
   elements['taskName'] = document.createElement("INPUT");
   elements['taskInfo'] = document.createElement("INPUT");
   elements['taskDate']= document.createElement("INPUT");
-  elements['submit']=  document.createElement("INPUT");
+  //elements['submit']=  document.createElement("INPUT");
 
   this.inputElements= elements;
 }
@@ -33,31 +57,43 @@ initializeElements(){
   this.inputElements['taskInfo'].setAttribute("type", "text");
   this.inputElements['taskInfo'].placeholder="task info(optional)";
   this.inputElements['taskDate'].setAttribute("type", "date");
-  this.inputElements['submit'].setAttribute("type", "submit");
+  // this.inputElements['submit'].setAttribute("type", "submit");
+  // this.inputElements['submit'].addEventListener('click',()=>App.pushNewElement(this.inputElements));
 }
 
 render(){
-  const formTEST = document.createElement('form');
-  formTEST.setAttribute("action","./");
-  const app = document.getElementById('App');
-  app.append(formTEST)
-  for (const field in this.inputElements){
-    formTEST.append(this.inputElements[field]);
-  } 
-
+  const form = document.createElement('section');
+  form.setAttribute("onclick",()=>alert('added'));
+  const container = document.getElementById(this.hookid);
+  for (const field in this.inputElements)
+    form.append(this.inputElements[field]);
+  const submitBtn = document.createElement("INPUT");
+  submitBtn.setAttribute("type", "submit");
+  submitBtn.addEventListener('click',()=>App.pushNewElement(this.inputElements));
+  form.append(submitBtn);
+  container.append(form)
+  
+  }
 
 }
-
-}
-
-
-
 
 
 class App {
 
   static init(){
-    new InputTask();
+    const app = document.getElementById("App");
+    const wrapper = document.createElement('div');
+    wrapper.id ="wrapper";
+    app.append(wrapper);
+
+    new InputTask('wrapper');
+  }
+
+  static pushNewElement(task){
+    const newItem = new TodoItem(task);
+    for (const field in task){
+    console.log(task[field].value);
+    }
   }
 }
 
